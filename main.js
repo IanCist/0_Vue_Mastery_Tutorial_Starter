dc2765ac78aae05e554ae55fa436c432ed4306b9
IanCist <iantcisternas@gmail.com>
2024-12-10 08:59:56 -0500

    Attribute Binding

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'socks',
-            description: 'A warm fuzzy pair of socks'
+         image:'./assets/images/socks_green.jpg'
         }
     }
 })
