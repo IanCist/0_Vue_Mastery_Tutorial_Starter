648d0a986f7de7e2b3e064f292d49981e5c22d92
IanCist <iantcisternas@gmail.com>
2024-12-10 09:13:06 -0500

    Conditional Rendering

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'socks',
-         image:'./assets/images/socks_green.jpg'
+         image:'./assets/images/socks_green.jpg',
+         inventory: 100
         }
     }
 })
